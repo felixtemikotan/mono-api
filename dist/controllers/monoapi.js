d89fcd0eba464280205e75fbba20b75d50bd94a7
dannack <felixtemikotan@yahoo.com>
2022-11-08 13:05:38 +0100

    mono api

--- dist/controllers/monoapi.js
+++ dist/controllers/monoapi.js
@@ -7,13 +7,6 @@ exports.getAllBanksNG = void 0;
 const axios_1 = __importDefault(require("axios"));
 const BASE_API_URL = 'https://api.withmono.com';
 const bankUrl = `${BASE_API_URL}/v1/institutions`;
-// const options:any = {
-//     'method': 'GET',
-//     'url': '{{BASE_API_URL}}/v1/institutions',
-//     'headers': {
-//       'Authorization': 'Bearer FLWSECK_TEST-SANDBOXDEMOKEY-X'
-//     }
-//   };
 const getAllBanksNG = async () => {
     try {
         const response = await axios_1.default.get(bankUrl);
--- src/controllers/monoapi.ts
+++ src/controllers/monoapi.ts
@@ -5,13 +5,6 @@ const BASE_API_URL = 'https://api.withmono.com'
 
 
 const bankUrl = `${BASE_API_URL}/v1/institutions`;
-// const options:any = {
-//     'method': 'GET',
-//     'url': '{{BASE_API_URL}}/v1/institutions',
-//     'headers': {
-//       'Authorization': 'Bearer FLWSECK_TEST-SANDBOXDEMOKEY-X'
-//     }
-//   };
 
 export const getAllBanksNG = async () => {
   try {
